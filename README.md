b36b0396ced3398f11efa3f137042d50d54e0909
Max <nuzghoul@gmail.com>
2022-03-04 15:24:42 +0400

    initial

---
+++ README.md
@@ -0,0 +1,18 @@
+# Тестовое задание от Appbooster
+[Описание задания](https://github.com/appbooster/test-assignments/blob/master/tasks/html-css.md)
+
+## Сборка
+
+Проект создан на основе [Webpack 5 Boilerplate Template](https://github.com/WeAreAthlon/frontend-webpack-boilerplate). 
+
+Для сборки нужно выполнить команду:
+
+```sh
+$ npm run production
+```
+
+Для запуска dev сервера:
+
+```sh
+$ npm run dev
+```
---
+++ src/js/app.js
@@ -0,0 +1,63 @@
+import '../scss/app.scss';
+
+// Handle table row toggling
+const nameItems = document.querySelectorAll('.table-data__row-body_expandable .table-data__cell-name');
+nameItems.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    e.stopPropagation();
+    const parentRow = e.target.closest('.table-data__row-body');
+    parentRow.classList.toggle('table-data__row-body_opened');
+  });
+});
+
+// Handle star button
+const btnFavoriteItems = document.querySelectorAll('.button-favorite');
+btnFavoriteItems.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    e.stopPropagation();
+    item.classList.toggle('button-favorite_active');
+  });
+});
+
+// Handle table columns sorting
+const tableSortHeading = document.querySelectorAll('.table-data__sort-direction');
+tableSortHeading.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    e.stopPropagation();
+    if (item.classList.contains('table-data__sort-direction_desc') || item.classList.contains('table-data__sort-direction_asc')) {
+      item.classList.toggle('table-data__sort-direction_desc');
+      item.classList.toggle('table-data__sort-direction_asc');
+    } else {
+      // if column's sort has no set direction
+      item.classList.add('table-data__sort-direction_desc');
+    }
+  });
+});
+
+// Handle same app hover highlighting
+const appIcons = document.querySelectorAll('.table-data__icon-app');
+const handleAppIconsHover = (element) => {
+  const { id } = element.target.dataset;
+  const sameElems = document.querySelectorAll(`.table-data__icon-app[data-id='${id}']`);
+  sameElems.forEach((elem) => {
+    elem.classList.toggle('table-data__icon-app_highlighted');
+  });
+};
+appIcons.forEach((item) => {
+  item.addEventListener('mouseover', (e) => {
+    handleAppIconsHover(e);
+  });
+});
+appIcons.forEach((item) => {
+  item.addEventListener('mouseleave', (e) => {
+    handleAppIconsHover(e);
+  });
+});
+
+const checkboxToggleAll = document.querySelector('input[type=checkbox][data-type=toggleAll]');
+const checkboxesTableItems = document.querySelectorAll('input[type=checkbox][data-type=tableItem]');
+checkboxToggleAll.addEventListener('input', (e) => {
+  checkboxesTableItems.forEach((item) => {
+    item.checked = e.target.checked;
+  });
+});
